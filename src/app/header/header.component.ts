3919684cdb81757d055df7dfc106ca95f25457ee
mfarnicki <mfarnicki@gmail.com>
2022-10-04 17:31:32 +0200

    Simple navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuCollapsed: boolean = true;
+  @Output() navigateTo: EventEmitter<string> = new EventEmitter<string>();
+
+  onNavigateTo(page: string) {
+    this.navigateTo.emit(page);
+  }
 }
