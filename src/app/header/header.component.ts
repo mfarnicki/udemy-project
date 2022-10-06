2bbbd7704ca69f2435bf18a712a408b4dc0ce6ba
mfarnicki <mfarnicki@gmail.com>
2022-10-06 21:44:23 +0200

    Anchor tags cleanup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuCollapsed: boolean = true;
-  @Output() navigateTo: EventEmitter<string> = new EventEmitter<string>();
-
-  onNavigateTo(page: string) {
-    this.navigateTo.emit(page);
-  }
 }
