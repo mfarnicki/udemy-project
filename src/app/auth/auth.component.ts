9064b03cfb8f9c5eac1de92aa57a59811e4868af
mfarnicki <mfarnicki@gmail.com>
2022-10-13 13:21:23 +0200

    Dynamic component - alert with a backdrop

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder.directive';
 import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = '';
+  @ViewChild(PlaceholderDirective, { static: false })
+  alertHost!: PlaceholderDirective;
+  private closeSub: Subscription = Subscription.EMPTY;
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -43,10 +54,27 @@ export class AuthComponent {
       },
       error: (err: Error) => {
         this.error = err.message;
+        this.showErrorAlert(err);
         this.isLoading = false;
       },
     });
 
     loginForm.reset();
+  }
+
+  onHandleError() {
+    this.error = '';
+  }
+
+  private showErrorAlert(error: Error) {
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const alertCmp = hostViewContainerRef.createComponent(AlertComponent);
+    alertCmp.instance.message = error.message;
+    this.closeSub = alertCmp.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
