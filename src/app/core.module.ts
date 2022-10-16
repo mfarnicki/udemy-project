b350d2123cfa76b8f4759443a7051f2af8c45380
mfarnicki <mfarnicki@gmail.com>
2022-10-16 18:22:35 +0200

    Remove ShoppingListService. Maintain shopping list state in a ngrx store

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,12 +2,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipeService } from './shared/recipe.service';
-import { ShoppingListService } from './shared/shopping-list.service';
 
 @NgModule({
   providers: [
     RecipeService,
-    ShoppingListService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,37 +1,29 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { LoggingService } from '../logging.service';
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shared/shopping-list.service';
+import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[] = [];
-  subscription: Subscription = Subscription.EMPTY;
+export class ShoppingListComponent implements OnInit {
+  ingredients!: Observable<fromShoppingList.State>;
 
   constructor(
-    private shoppingListService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients) => (this.ingredients = ingredients)
-    );
-
+    this.ingredients = this.store.select('shoppingList');
     this.loggingService.printLog('Hello from ShoppingListComponent');
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
   onEditItem(index: number) {
-    this.shoppingListService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 }
